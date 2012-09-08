bea3683706b3800d52020e3b64c0e7301d827fc8
Steve Losh <steve@stevelosh.com>
2012-09-08 14:09:27 -0400

    Fix packaging script to include syntax files.

--- package.sh
+++ package.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-hg archive ~/Desktop/clam.zip -I 'doc' -I 'plugin' -I README.markdown -I LICENSE.markdown
+hg archive ~/Desktop/clam.zip -I 'doc' -I 'plugin' -I 'clamsyntax' -I README.markdown -I LICENSE.markdown
